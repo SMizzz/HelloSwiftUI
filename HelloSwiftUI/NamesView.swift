53a2032d7e47a88ab841223b382c27cb5cf83d56
SMizzz <shinn.mizzz@gmail.com>
2022-10-18 17:58:16 +0900

    add ViewModifier : 활용하여 코드 경량화

--- HelloSwiftUI/NamesView.swift
+++ HelloSwiftUI/NamesView.swift
@@ -13,11 +13,13 @@ struct NamesView: View {
             Text("👼🏻")
                 .font(.largeTitle)
             Text("Ethan")
+                .modifier(StandardTextShadowColor())
             HStack {
                 VStack {
                     Text("🙍🏻")
                         .font(.largeTitle)
                     Text("Kyle")
+                        .modifier(StandardTextShadowColor())
                 }
                 .padding()
                 
@@ -25,6 +27,7 @@ struct NamesView: View {
                     Text("🙍🏻")
                         .font(.largeTitle)
                     Text("Beom")
+                        .modifier(StandardTextShadowColor())
                 }
                 .padding()
                 
@@ -32,6 +35,7 @@ struct NamesView: View {
                     Text("🙎🏻‍♀️")
                         .font(.largeTitle)
                     Text("Hyem")
+                        .modifier(StandardTextShadowColor())
                 }
                 .padding()
             }
@@ -41,6 +45,7 @@ struct NamesView: View {
                     Text("🙎🏻‍♀️")
                         .font(.largeTitle)
                     Text("Jasmin")
+                        .modifier(StandardTextShadowColor())
                 }
                 .padding()
                 
@@ -48,7 +53,8 @@ struct NamesView: View {
                     Text("🙎🏻‍♀️")
                         .font(.largeTitle)
                     Text("Jero")
-                        .foregroundColor(.purple)
+                        .modifier(StandardTextColor())
+                        .modifier(StandardTextShadowColor())
                 }
                 .padding()
                 
@@ -56,12 +62,35 @@ struct NamesView: View {
                     Text("🙍🏻")
                         .font(.largeTitle)
                     Text("Young")
-                }
-                .padding()
+                        .modifier(StandardTextShadowColor())
+                }.padding()
             }
         }
-        .font(.title)
-        .bold()
+        .modifier(StandardName())
+    }
+}
+
+struct StandardName: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title2)
+            // 커스텀 폰트 방법
+    //        .font(.custom("", size: .title))
+            .bold()
+    }
+}
+
+struct StandardTextColor: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(Color(CGColor(red: 115/255, green: 85/255, blue: 255/255, alpha: 1)))
+    }
+}
+
+struct StandardTextShadowColor: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: .yellow, radius: 15, x: 5 , y: 5)
     }
 }
 
--- HelloSwiftUI/TitleView.swift
+++ HelloSwiftUI/TitleView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct TitleView: View {
+    func sayHello() {
+        print("안녕하세요")
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "crown.fill")
-                .resizable()
-                .frame(width: 60, height: 50)
-                .foregroundColor(.purple)
+            Button("Hello") {
+                print("Hello")
+            }
+            
+            Button("Hi", action: {
+                print("Hi")
+            })
+            
+            Button("인사하기 클로저", action: sayHello)
+                .padding()
+            
+            Button(action: {
+                print("인사해주세요")
+            }) {
+                Image(systemName: "hands.sparkles")
+                    .renderingMode(.original)
+                    .resizable()
+                // 이미지 리사이즈 방법
+                // #1 이런 식으로 aspectRatio와 padding을 함께 쓰는 방법이 있고,
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .padding()
+                    .padding()
+                // #2 사이즈를 정하는 방법이 있다.
+                //                    .frame(width: 60, height: 50)
+                //                    .modifier(StandardTextColor())
+                
+            }
+            
             Text("Hello No.1")
                 .font(.largeTitle)
                 .bold()
                 .underline()
-                .padding()
+            Button("인삿말") {
+                sayHello()
+            }
+            .padding()
         }
     }
 }
