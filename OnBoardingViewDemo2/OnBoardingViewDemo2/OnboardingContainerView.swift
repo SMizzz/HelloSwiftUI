8cb621c34d00414bac1fb337f5bda79bda0f07e8
Jero <62836016+SMizzz@users.noreply.github.com>
2022-11-15 23:29:14 +0900

    onBoardingView Edited.

--- OnBoardingViewDemo2/OnBoardingViewDemo2/OnboardingContainerView.swift
+++ OnBoardingViewDemo2/OnBoardingViewDemo2/OnboardingContainerView.swift
@@ -8,15 +8,63 @@
 import SwiftUI
 
 struct OnboardingContainerView: View {
-    var body: some View {
-        TabView {
-            ForEach(features) { feature in
-                OnboardingContentView(feature: feature)
-            }
-        }
-        .tabViewStyle(PageTabViewStyle())
-        .edgesIgnoringSafeArea(.all)
-    }
+    @State private var selectedPage = 0
+       var body: some View {
+           VStack {
+               HStack {
+                   Button {
+                       if selectedPage > 0 {
+                           withAnimation { selectedPage -= 1 }
+                       }
+                   } label: {
+                       Image(systemName: "chevron.left")
+                   }
+                   
+                   TabView(selection: $selectedPage) {
+                       Text("Hi").tag(0)
+                       Text("Hello").tag(1)
+                       Text("Welcome").tag(2)
+                   }
+                   .tabViewStyle(PageTabViewStyle())
+                   .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                   
+                   Button {
+                       if selectedPage < 2 {
+                           withAnimation { selectedPage += 1 }
+                       }
+                   } label: {
+                       Image(systemName: "chevron.right")
+                   }
+                   
+//                   Button("<") { if selectedPage > 0 {
+//                       withAnimation { selectedPage -= 1 }
+//                   } }
+//                   Spacer().frame(width: 40)
+//                   Button(">") { if selectedPage < 2 {
+//                       withAnimation { selectedPage += 1 }
+//                   } }
+               }
+//               TabView(selection: $selectedPage) {
+//                   Text("Hi").tag(0)
+//                   Text("Hello").tag(1)
+//                   Text("Welcome").tag(2)
+//               }
+//               .tabViewStyle(PageTabViewStyle())
+//               .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+           }
+       }
+//    var body: some View {
+//        VStack {
+//
+//            TabView {
+//                ForEach(features) { feature in
+//                    OnboardingContentView(feature: feature)
+//                }
+//            }
+//            .tabViewStyle(PageTabViewStyle())
+//            .edgesIgnoringSafeArea(.all)
+//        }
+//    }
 }
 
 struct OnboardingContainerView_Previews: PreviewProvider {
