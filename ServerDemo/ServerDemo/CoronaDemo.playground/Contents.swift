b5a867cbe2953a77cb68d943a59e950738e208c2
Jero <62836016+SMizzz@users.noreply.github.com>
2022-11-22 14:51:55 +0900

    Fix: Edit CenterData Model And Added Another Way.

--- ServerDemo/ServerDemo/CoronaDemo.playground/Contents.swift
+++ ServerDemo/ServerDemo/CoronaDemo.playground/Contents.swift
@@ -4,19 +4,19 @@ import Foundation
 
 struct CoronaClinicData: Codable {
     var currentCount: Int
-    var data: [ClinicData]
+    var data: [CenterData]
     var matchCount: Int
     var page: Int
     var perPage: Int
     var totalCount: Int
 }
 
-struct ClinicData: Codable {
+struct CenterData: Codable {
     var address: String
     var centerName: String
     var centerType: String
     var createdAt: String
-    var facillityName: String
+    var facilityName: String
     var id: Int
     var lat: String
     var lng: String
@@ -30,8 +30,8 @@ struct ClinicData: Codable {
 
 let url = URL(string: "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=10&serviceKey=IqV8U3oL39Xq5A1gSbKYKHnRESAPgGu397bHbKxmiAlqiHoR1Zkf5yNLne8Xtc%2B9MEt8XKceRDvE%2F%2Bu4%2Fn6rbQ%3D%3D")!
 
-// singleton 객체 하나로만 사용하겠다.?
-// url을 가지고 data를 가져오거든 후행 클로저에 있는 일을 실행하라.
+// #1
+/*
 let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
     // data, response, error를 활용한 데이터 가져온 이후의 작업
     
@@ -49,12 +49,7 @@ let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
         return
     }
     */
-    
-    // 그저 UTF-8로 변환한 String Data를 보기 위한 코드
-    let showingData = String(data: data, encoding: .utf8)!
-    print("\(showingData)")
-    
-    
+   
     // JsonDecoder()를 사용하여 Json -> Swift에서 사용할 수 있게 변환해준다.
     do {
         // JSON으로 받은 데이터를 [Car] 모양으로 해독해주는 작업을 해주었다.
@@ -70,4 +65,64 @@ let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
         print(error.localizedDescription)
     }
 }
+task.resume()
+*/
+
+// #2
+/*
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    // data, response, error를 활용한 데이터 가져온 이후의 작업
+    
+    // data가 nil일 경우 옵셔널 바인딩에서 else 처리된다면 끝
+    guard let data else {
+        print("nothing")
+        print("\(error?.localizedDescription)")
+        return
+    }
+    
+    do {
+        let results = try JSONDecoder().decode(CoronaClinicData.self, from: data)
+        for item in results.data {
+            print(item)
+        }
+    } catch {
+        print(error.localizedDescription)
+    }
+}.resume()
+*/
+
+// #3
+func loadJson<T: Decodable>(data: Data) -> T {
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Unable to parse data: \(error)")
+    }
+}
+
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    guard let data else {
+        print("data - not found")
+        return
+    }
+    
+    let dataToString = String(data: data, encoding: .utf8)!
+    print("\(dataToString)")
+    
+    let centers: CoronaClinicData = loadJson(data: data)
+    print("\(centers)")
+    print("currentCount: \(centers.currentCount)")
+    print("matchCount : \(centers.matchCount)")
+    
+    let centerList = centers.data
+    
+    for center in centerList {
+        print("\(center.centerName)")
+        print("\(center.sido)")
+        print("\(center.address)")
+        print("\(center.centerType)")
+        print("------------------------")
+    }
+}
+
 task.resume()
